3b7b47d90ffc29304d535c8a9c92b9bc094fee1d
Fabian Eilber <fabian_eilber@arburg.com>
2023-04-26 10:11:35 +0200

    Exceptions für BCGenerator eingefügt

--- src/main/java/bytecode/exceptions/InvalidStatementException.java
+++ src/main/java/bytecode/exceptions/InvalidStatementException.java
@@ -1,5 +1,15 @@
 package bytecode.exceptions;
 
-public class InvalidStatementException
+public class InvalidStatementException extends RuntimeException
 {
+    public InvalidStatementException(String _message)
+    {
+        super(_message);
+    }
+
+    public InvalidStatementException(String _message, Throwable _error)
+    {
+        super(_message, _error);
+    }
 }
+
--- src/main/java/bytecode/exceptions/InvalidStatementExpressionException.java
+++ src/main/java/bytecode/exceptions/InvalidStatementExpressionException.java
@@ -1,5 +1,14 @@
 package bytecode.exceptions;
 
-public class InvalidStatementExpressionException
+public class InvalidStatementExpressionException extends RuntimeException
 {
+    public InvalidStatementExpressionException(String _message)
+    {
+        super(_message);
+    }
+
+    public InvalidStatementExpressionException(String _message, Throwable _error)
+    {
+        super(_message, _error);
+    }
 }

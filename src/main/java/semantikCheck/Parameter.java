dff543c1b0b0433b5ba75653819ac8e2d794731a
Timo Zink <zink.timo@protonmail.com>
2023-05-03 13:44:58 +0200

    added Parameter

---
+++ src/main/java/semantikCheck/Parameter.java
@@ -0,0 +1,11 @@
+package semantikCheck;
+
+public class Parameter {
+    private Type type;
+    private String name;
+
+    public Parameter(Type type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+}

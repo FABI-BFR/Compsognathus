2a42c066697870f8939b8a6038a75e3afe1ea901
Timo Zink <zink.timo@protonmail.com>
2023-05-03 13:10:25 +0200

    added Content for Method

--- src/main/java/semantikCheck/Method.java
+++ src/main/java/semantikCheck/Method.java
@@ -1,4 +1,39 @@
 package semantikCheck;
 
+import semantikCheck.interfaces.IExpr;
+import semantikCheck.stmt.Block;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Method {
+
+    private Type type;
+    private String name;
+    private List<IExpr> parameter;
+    private Block statement;
+    private Access access;
+
+    public Method (Type type, String name, List<IExpr> parameter, Block statement) {
+        this(type, name, parameter, statement, Access.PUBLIC);
+    }
+
+    public Method (Type type, String name, List<IExpr> parameter, Block statement, Access access) {
+        this.type = type;
+        this.name = name;
+        this.parameter = new ArrayList<>();
+        if (parameter != null && !parameter.isEmpty()) {
+            this.parameter.addAll(parameter);
+        }
+        this.statement = statement;
+        if (access == null) {
+            access = Access.PUBLIC;
+        }
+        else
+        {
+            this.access = access;
+        }
+    }
+
+
 }

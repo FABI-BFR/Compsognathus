e331edc32b501ceb30509959ed69db25400bf4ed
Timo Zink <zink.timo@protonmail.com>
2023-05-03 14:57:11 +0200

    Added Content to LocalVarDecl, Return und While

--- src/main/java/semantikCheck/stmt/While.java
+++ src/main/java/semantikCheck/stmt/While.java
@@ -1,4 +1,30 @@
 package semantikCheck.stmt;
 
+import semantikCheck.interfaces.IExpr;
+import semantikCheck.interfaces.IStmt;
+
 public class While {
+    private IExpr expression;
+    private IStmt statement;
+
+    public While (IExpr expression, IStmt statement) {
+        this.expression = expression;
+        this.statement = statement;
+    }
+
+    public IExpr getExpression() {
+        return expression;
+    }
+
+    public void setExpression(IExpr expression) {
+        this.expression = expression;
+    }
+
+    public IStmt getStatement() {
+        return statement;
+    }
+
+    public void setStatement(IStmt statement) {
+        this.statement = statement;
+    }
 }

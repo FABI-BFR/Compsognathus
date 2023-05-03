4ceb3a13a877b63d904cbd95c115c86a922f4ae2
Timo Zink <zink.timo@protonmail.com>
2023-05-03 12:58:59 +0200

    added Content to Field

--- src/main/java/semantikCheck/Field.java
+++ src/main/java/semantikCheck/Field.java
@@ -1,4 +1,22 @@
 package semantikCheck;
 
 public class Field {
+    private String type;
+    private String name;
+    private Access access;
+
+    public Field (String type, String name){
+        this(type, name, Access.PUBLIC);
+    }
+
+    public Field (String type, String name, Access access) {
+        this.type = type;
+        this.name = name;
+        if(access == null){
+            access = Access.PUBLIC;
+        }
+        else {
+            this.access = access;
+        }
+    }
 }

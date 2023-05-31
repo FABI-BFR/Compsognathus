dabbfc0e2257bf6f1e62cbfc39bbbe920ec9407c
Timo Zink <zink.timo@protonmail.com>
2023-05-31 13:39:23 +0200

    Changed expression to object

--- src/main/java/semantikCheck/stmtexpr/MethodCall.java
+++ src/main/java/semantikCheck/stmtexpr/MethodCall.java
@@ -15,20 +15,20 @@ import java.util.List;
 public class MethodCall implements IStmtExpr {
 
     private Type type;
-    public IExpr expression;
+    public IExpr object;
     public String name;
     public Method method;
     public boolean methodStored;
     public List<IExpr> parameters;
 
-    public MethodCall(IExpr expression, String name, List<IExpr> parameters) {
-        this.expression = expression;
+    public MethodCall(IExpr object, String name, List<IExpr> parameters) {
+        this.object = object;
         this.name = name;
         this.parameters = parameters;
     }
 
     public MethodCall(IExpr expression, String name, List<IExpr> parameters, boolean stored) {
-        this.expression = expression;
+        this.object = expression;
         this.name = name;
         parameters = new ArrayList<>();
         if(parameters != null && !parameters.isEmpty()){
@@ -55,15 +55,15 @@ public class MethodCall implements IStmtExpr {
     @Override
     public void semCheck(List<Parameter> parameters, List<Class> classes, Class currentClass)
     {
-        if (expression == null) {
-            expression = new This();
+        if (object == null) {
+            object = new This();
         }
-        expression.semCheck(parameters, classes, currentClass);
+        object.semCheck(parameters, classes, currentClass);
         this.parameters.forEach(p -> p.semCheck(parameters, classes, currentClass));
-        if (!(expression instanceof LocalOrFieldVar) || (expression instanceof This) || (expression instanceof Super)) {
+        if (!(object instanceof LocalOrFieldVar) || (object instanceof This) || (object instanceof Super)) {
             Checker.addDereferenceError(currentClass.getName(), type);
         }
-        Type classType = expression.getType();
+        Type classType = object.getType();
         Class tempClass = null;
         for(Class c : classes) {
             if(c.getName().equals(classType.toString())) {
@@ -88,7 +88,7 @@ public class MethodCall implements IStmtExpr {
 
         List<Method> accessableMethods = new ArrayList<>();
         for (Method m : tempMethods) {
-            if(m.getAccess() == Access.PUBLIC || expression instanceof This) {
+            if(m.getAccess() == Access.PUBLIC || object instanceof This) {
                 accessableMethods.add(m);
             }
         }
